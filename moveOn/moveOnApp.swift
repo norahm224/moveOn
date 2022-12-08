8bc4bc428dc8d4e49e8571d7c4680a5fac257893
norahm224 <norah.213113778@gmail.com>
2022-12-08 09:52:53 +0300

    Initial Commit

---
+++ moveOn/moveOnApp.swift
@@ -0,0 +1,17 @@
+//
+//  moveOnApp.swift
+//  moveOn
+//
+//  Created by Nourah Almusaad on 08/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct moveOnApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
